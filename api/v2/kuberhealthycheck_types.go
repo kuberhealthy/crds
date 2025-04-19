7d0e16fe04e05aa98e7207913dc1ce91481478b1
integrii <integrii@gmail.com>
2025-04-19 01:12:43 -0700

    preserve unknown fields

--- api/v2/kuberhealthycheck_types.go
+++ api/v2/kuberhealthycheck_types.go
@@ -24,6 +24,7 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+// +kubebuilder:validation:XPreserveUnknownFields
 // KuberhealthyCheckSpec defines the desired state of KuberhealthyCheck
 type KuberhealthyCheckSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
